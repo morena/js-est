8eca9bce0da44ecd17c96d9849640915d5c42370
Morena Fiore <info@morenafiore.com>
2015-06-10 15:46:58 +0100

    New iteration adding Mustache

--- test/js/validate-new.js
+++ test/js/validate-new.js
@@ -11,16 +11,6 @@ Validator.prototype = {
 	//establish the rules for validating the fields
 	rules: {
 		alphanumeric: function($textField){
-			/*var value = $textField.val(),
-				pattern = new RegExp(/[^a-zA-Z0-9\d\s]/g);
-
-			//check the field is not empty
-			if(value.length === 0){
-				return false;
-			}
-			var invalidChars = pattern.test(value);
-			//this is bad, I know but is there a better way?
-			return !invalidChars;*/
 
 			return !!$textField.val().match(/^[a-z\d\s]+$/);
 		},
@@ -86,103 +76,29 @@ var DataPopulator = function($divToPopulate){
 
 DataPopulator.prototype = {
 
-	getMarkup: function(callback){
-		//get the markup template file
-		$.ajax({
-			type: "GET",
-			dataType: "html",
-			url: "../data/template.html"
-		})
-		.done(function(data){
-		 	callback(data);
-		})
-		.fail(function(){
-			alert("Sorry there was an error.");
-		});
-	},
-
 	makeRequest: function(){
 
 		var dataPopulator = this;
 
-		/*$.ajax({
-			type: "GET",
-			dataType: "json",
-			url: "../data/test.json"
-		})
-		.done(function(data){
-			//we get the div template
-			//console.log(dataPopulator.getMarkup());
-			dataPopulator.getMarkup(function(divTemplate){
-				dataPopulator.populateData(divTemplate, data.panels);
-			});
-
-		})
-		.fail(function(){
-			alert("Sorry there was an error.");
-		});*/
-
-
-		//multiple ajax requests
-		$.when($.ajax({url: "../data/test.json"}),$.ajax({url: "../data/template.mst"}))
-		.done(function(data, divTemplate){
-			dataPopulator.populateData(divTemplate, data[0].panels);
+		$.when($.ajax({url: "../data/template.mst", dataType: 'text'}),$.ajax({url: "../data/test.json"}))
+		.done(function(template, data){
+			dataPopulator.populateData(template[0], data[0].panels);
 		})
 		.fail(function(){
 			alert("Sorry there was an error.");
 		});
 	},
 
-	populateData: function(divTemplate, panels){
+	populateData: function(template, panels){
 		//save each item to populate
-		var titleFromTemplate,
-			descFromTemplate,
-			ulFromTemplate,
-			titleFromPanel,
-			descFromPanel,
-			populatedLinks,
-			divToPopulate = this.$divToPopulate,
-			rendered,
-			text;
+		//console.log(template);
+		console.log(panels);
+		Mustache.parse(template);
+		var rendered = Mustache.to_html(template, panels);
+		//this.$divToPopulate.html(rendered);
+		this.$divToPopulate.append(rendered);
 
-		//iterate through the array of panels
-		$.each(panels, function(k, v){ //this is the jQuery each
 
-			//$(divToPopulate).append(divTemplate);
-
-			//select the item that corresponds to each of the previous items
-			/*titleFromTemplate =  $(divToPopulate).children().last("div").find(".title");
-			descFromTemplate =  $(divToPopulate).children().last("div").find(".desc");
-			ulFromTemplate =  $(divToPopulate).children().last("div").find(".links");
-
-			//save each item to populate
-
-			titleFromPanel = "Panel"+v.id;
-			descFromPanel = v.desc;
-			populatedLinks = '';
-
-			//iterate through the links and put them together into an <ul>
-			$.each(v.links, function(l, b){
-				populatedLinks += '<li><a href="'+b.url+'">'+b.title+'</a></li>';
-			});
-
-			$(titleFromTemplate).html(titleFromPanel);
-			$(descFromTemplate).html(descFromPanel);
-			$(ulFromTemplate).html(populatedLinks);*/
-
-			//Mustache.js version
-			text = {
-				titleFromPanel: "Panel"+v.id,
-				descFromPanel: v.desc
-			};
-
-			$(divToPopulate).append('<div class="resultItem" id="'+k+'"></div>');
-
-			rendered = Mustache.to_html(divTemplate, text);
-
-			$('#'+k).html(rendered);
-
-		});
 	}
 
 };
