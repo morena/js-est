e0f154752e1c8992b7ecd609dd87dd5a9b30d0b1
Morena Fiore <morenafiore@gmail.com>
2015-07-13 15:29:25 +0100

    One code war solved

--- code-wars/5514e5b77e6b2f38e0000ca9.js
+++ code-wars/5514e5b77e6b2f38e0000ca9.js
@@ -17,32 +17,31 @@ Test.assertSimilar(upArray(
   [ 9, 2, 2, 3, 3, 7, 2, 0, 3, 6, 8, 5, 4, 7, 7, 5, 8, 0, 7 ]),
   [9,2,2,3,3,7,2,0,3,6,8,5,4,7,7,5,8,0,8]));
 */
-
 function isAnInt(n){
   return n === Number(n);
 }
-function composeArrayValue (arrayValue, i){
-  var numbeToString = i.toString();
-  arrayValue = arrayValue+numbeToString;
-
-  return arrayValue;
-}
 function upArray(arr){
-  //console.log(arr);
+  console.log(arr);
   
   if(arr.length === 0){
     return null;
   }
-	var arrayLength = arr.length-1,
-      isInt = false,
-      breakCheck1 = false,
-      arrayValue = '',
-      numbeToString = '',
-      newArray = [],
-      finalArray = [];
+  //increase the last item
+  var lastItem = arr.pop(); 
+  if(lastItem >= 0 ){
+    lastItem+=1;
+    arr.push(lastItem);
+   }else{
+     return null;
+   }
   
-	for(var i = arrayLength; i >=0; i--){
-    isInt = isAnInt(arr[i]);
+  for (var i = arr.length - 1; i >= 0; i--) {
+    //console.log(arr);
+    //console.log("current value is "+arr[i]);
+
+    var isInt = isAnInt(arr[i]),
+        breakCheck1 = false;
+        
     if(arr[i] < 0 || arr[i] >10 || isInt === false){
       breakCheck1 = true;
     }
@@ -50,103 +49,17 @@ function upArray(arr){
       return null;
       break;
     }
-
-
-    if(arr[i] == 9){
-      //console.log(i);
-        //console.log("arr["+i+"] is a 9");
-    	arr[i] = 0;
-	    arrayValue = composeArrayValue (arrayValue, arr[i]);
-        //console.log("arrayValue is "+arrayValue);
-        if(arr[i-1] == undefined){
-          arr[i-1] = 1;
-        }else{
-          arr[i-1] = arr[i-1]+1;
-        }
-        //console.log("eccola "+arr);
-        //console.log("yo "+i);
-        i--;
-        break;
-        //console.log("arr[i-1] is now "+arr[i-1]);
-    }else if(arr[i] == 10){
-      console.log(i);
-      console.log("it is a 10");
-		  arr[i] = 1;
-      console.log(arr);
-	    arrayValue = composeArrayValue (arrayValue, arr[i]);
-      console.log("arrayValue is "+arrayValue);
-      if(arr[i-1] == undefined){
-        arr[i-1] = 1;
-      }else{
-        arr[i-1] = arr[i-1]+1;
-      }
-      console.log("yo "+i);
-      i--;
-      break;
-
-    }else{
-      //console.log(i);
-        //console.log("arr["+i+"] is a  "+arr[i]);
-        arr[i] = arr[i]+1;
-        //console.log("arr[i] (arr["+i+"]) is now "+arrayValue);
-	    arrayValue = composeArrayValue (arrayValue, arr[i]);
-        //console.log("arrayValue is "+arrayValue);
-    }
     
-    if (breakCheck1) {
-      return null;
-      break;
+    if(arr[i] == 10){
+      arr[i] = 0;
+      if(arr[i-1] == undefined){
+        arr.unshift(1);
+      }else{
+        arr[i-1]+=1;
+      }
+      //console.log("now array is "+arr);
     }
-	}
-  
-  newArray = (""+arrayValue).split("");
-  newArray.reverse();
-  for(var b = 0; b < newArray.length; b++){
-    finalArray.push(parseInt(newArray[b]));
-   }
-  return finalArray;
+  };
+
+  return arr;
 }
-
-upArray([2,3,9]), [2,4,0];
-upArray([1,-9]), null
-
-
-
-/*function isAnInt(n){
-  return n === Number(n);
-}
-function upArray(arr){
-  console.log(arr);
-  if(arr.length === 0){
-    return null;
-  }
-  var arrayValue = '',
-      newArrayAsString = [],
-      newArray = [],
-      numbeToString = '',
-      newArrayValue = 0,
-      breakCheck1=false,
-      isInt = true,
-      arrayValueAsInt= 0;
-  for(var i = 0; i < arr.length ; i++){
-    isInt = isAnInt(arr[i]);
-    if(arr[i] < 0 || arr[i] >=10 || isInt === false){
-      breakCheck1 = true;
-    }
-    if (breakCheck1) {
-      break;
-    }
-    numbeToString = arr[i].toString();
-    arrayValue = arrayValue+numbeToString;
-  }
-  if (breakCheck1) {
-    return null;
-  }
-  arrayValueAsInt = parseInt(arrayValue);
-  newArrayValue = arrayValueAsInt+1;
-  newArrayAsString = (""+newArrayValue).split("");
-  for(var b = 0; b < newArrayAsString.length; b++){
-    newArray.push(parseInt(newArrayAsString[b]));
-   }
-  return newArray;
-}*/
